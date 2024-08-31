a3509d40de5f782d75dcd249dd6bd106eb088c06
ditinagrawal <ditin.agrawal05@gmail.com>
2024-08-31 22:03:29 +0530

    Add light/dark mode

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/shared/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <Button>Shadcn Button</Button>
+      <ModeToggle />
     </main>
   );
 }
